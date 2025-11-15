1dba3432ab142d2b1a1302c3e565c216e12f133a
Carlo Tran <carlotran4@gmail.com>
2025-11-15 16:39:27 -0600

    don't duplicate usernames

--- backend/src/signup.py
+++ backend/src/signup.py
@@ -2,11 +2,20 @@ import sqlite3
 
 def signup(name, email, password):
     try:
+        user_exists = f"""
+            select count(*) from user where email = '{email}';
+        """
         insert = """
             insert into user (email, password, name) VALUES (?, ?, ?);
         """
         conn = sqlite3.connect("./src/database.db")
         cursor = conn.cursor()
+        
+        cursor.execute(user_exists)
+        count = cursor.fetchone()[0]
+        print(count)
+        if(count > 0):
+            raise Exception('User already exists')
         cursor.execute(insert, (email, password, name))
         conn.commit()
     finally:
