7d6469bfb2375981f94209f7fe265616e762e7ac
Kyle Chi <kylechi2157@gmail.com>
2025-11-15 13:34:12 -0600

    frontend update

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function NavBar() {
     return (
-        <div className="flex flex-row items-center h-16 bg-(--background) text-(--light-gray) px-32 gap-20 transition-all">
+        <div className="flex flex-row items-center h-16 bg-(--background)/80 text-(--light-gray) px-32 gap-20 transition-all border-b border-(--primary-border-color)">
             <Link href="/" className="text-(--off-white) text-xl">TutorLink</Link>
             <Link href="/events" className="hover:text-(--off-white)">Events</Link>
             <Link href="/auth" className="hover:text-(--off-white) ml-auto">Sign in</Link>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <div className="flex">
-            <main className="">
-                <div>Home Page</div>
+            <main className="w-full bg-(--background) text-(--off-white)">
+                <div className="flex flex-col gap-18 px-40 py-30">
+                    <h1 className="text-6xl font-medium">
+                        Live tutoring that meets you anywhere
+                    </h1>
+                    <Link
+                        href="/events"
+                        className="cursor-hover w-fit rounded-full border-2 border-(--off-white) px-5 py-1"
+                    >
+                        Connect Now
+                    </Link>
+                </div>
             </main>
         </div>
     );
