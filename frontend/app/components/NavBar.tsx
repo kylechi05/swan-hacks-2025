c63e19b9847c9b941f071f6a8829917314a70798
Kyle Chi <kylechi2157@gmail.com>
2025-11-15 12:34:54 -0600

    temp

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,7 +1,9 @@
 export function NavBar() {
     return (
-        <div>
-            NAVBAR
+        <div className="flex flex-row items-center h-16 bg-blue-200 px-20 gap-12">
+            <div>NAVBAR</div>
+            <div>Events</div>
+            <div className="ml-auto">Sign in</div>
         </div>
     )
 }
