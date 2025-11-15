145c3dcd2136434dd85740ca7b10f8030023d172
Carlo Tran <carlotran4@gmail.com>
2025-11-15 13:50:40 -0600

    updating gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, Response, request, jsonify
 import src.videochat
+from src.login import login
+from src.signup import signup
 
 
 # Create a Flask app instance
@@ -22,5 +24,27 @@ def video_feed():
     return Response(src.videochat.gen(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/signup', methods=['POST'])
+def post_signup():
+    name = request.json.get('name')
+    email= request.json.get('email')
+    password = request.json.get('password')
+    try:
+        signup(name, email, password)
+        return 200
+    except Exception as e:
+        return {'error': f'Something went wrong - {e}'}, 500
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    email= request.json.get('email')
+    password = request.json.get('password')
+    user_id = login(email, password)
+    if user_id is None:
+        return {'error': 'Incorrect username or password'}, 401
+    return {'user_id': user_id}, 200
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=6969)
+
+
