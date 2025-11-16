f6ab48f8a8e0730c4a7613f6cfa5cc1a5a03c805
Garrett Kurth <garrett.kurth@caseys.com>
2025-11-16 00:45:55 -0600

    fix: please

--- frontend/app/meeting/[id]/page.tsx
+++ frontend/app/meeting/[id]/page.tsx
@@ -25,23 +25,11 @@ export default function MeetingPage() {
     const configuration: RTCConfiguration = {
         iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
-            { urls: "stun:stun1.l.google.com:19302" },
-            { urls: "stun:stun2.l.google.com:19302" },
             {
                 urls: "turn:numb.viagenie.ca",
                 username: "webrtc@live.com",
                 credential: "muazkh",
             },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=udp',
-      username: '28224511:1379330808',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=tcp',
-      username: '28224511:1379330808',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA='
-    }
         ],
         iceCandidatePoolSize: 10,
         bundlePolicy: "max-bundle",
