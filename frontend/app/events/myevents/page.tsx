0cd5e91340fdc29b7376990091e3a25ea3d0495c
Kyle Chi <kylechi2157@gmail.com>
2025-11-16 01:26:09 -0600

    fixing broken stuff

--- frontend/app/events/myevents/page.tsx
+++ frontend/app/events/myevents/page.tsx
@@ -39,7 +39,7 @@ export default function MyEvents() {
 
                 const data = await res.json();
                 const events = data.events;
-                console.log(events)
+                console.log("TUTEE", events)
 
                 setPendingTuteeEvents(
                     events.filter(
@@ -74,6 +74,7 @@ export default function MyEvents() {
 
                 const data = await res.json();
                 const events = data.events;
+                console.log(events)
 
                 setPendingTutorEvents(
                     events.filter(
@@ -86,7 +87,7 @@ export default function MyEvents() {
 
                 setAcceptedTutorEvents(
                     events.filter(
-                        (event) => event.userid_tutor === user.userid,
+                        (event) => event.userid_tutor?.userid_tutor === user.userid,
                     ),
                 );
             } catch (err: any) {
@@ -243,9 +244,9 @@ export default function MyEvents() {
                                         <EventDisplay
                                             event={event}
                                             startTime={
-                                                event.available_start_time
+                                                event.userid_tutor.start
                                             }
-                                            endTime={event.available_end_time}
+                                            endTime={event.userid_tutor.end}
                                         />
                                     </li>
                                 ))}
